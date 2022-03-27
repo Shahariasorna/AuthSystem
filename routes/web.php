ce219361d651fa5c38ba11ec677a942d118a13ac
Shaharia islam <Shahariaislam2@gmail.com>
2022-03-28 02:04:35 +0600

    create route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\backend\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('template.master');
 });
+
+Route::resource('/user', UserController::class);
